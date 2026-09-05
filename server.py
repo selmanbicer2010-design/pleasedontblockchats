f1edd8b73ac7329ebaeb987a9035accb6ee633c7
selmanbicer2010-design <selmanbicer2010@gmail.com>
2026-09-05 00:54:47 -0400

    aaa

--- server.py
+++ server.py
@@ -1,45 +1,29 @@
 import json
-from sqlite3.dbapi2 import connect as sqconnect
+#from sqlite3.dbapi2 import connect as sqconnect
 
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 
-class Server:
+class Event:
     def __init__(self):
-        self.app = FastAPI()
-        self.messagehistory = []
-        self.connectedclients = {}
-        self.deadclients = []
+        self.callbacks = {}
+        self.currentid_ = 0
 
-    def connect(self, client):
-        self.connectedclients[client.websocket] = client
+    def connect(self, callback):
+        self.currentid_ += 1
+        id = self.currentid_
+        self.callbacks[id] = callback
+        return id
 
-    def disconnect(self, client):
-        del self.connectedclients[client.websocket]
+    def disconnect(self, id):
+        if id in self.callbacks:
+            del self.callbacks[id]
 
-    def killclient(self, client):
-        self.deadclients.append(client)
-
-    def cleanclients(self):
-        for dead in self.deadclients:
-            self.disconnect(dead)
-        self.deadclients.clear()
-
-server = Server()
-app = server.app
-
-class Client:
-    def __init__(self, websocket, uuid, username):
-        self.websocket = websocket
-        self.uuid = uuid
-        self.username = username
-
-    async def sendtext(self, message):
-        await self.websocket.send_text(message)
-
-    async def receivedtext(self):
-        return await self.websocket.receive_text()
+    async def fire(self, *args):
+        callbacks = dict(self.callbacks)
+        for callback in list(callbacks.values()):
+            await callback(*args)
 
 class ServerSideClientTextInfo:
     def __init__(self, msgtype, datadict):
@@ -55,40 +39,114 @@ class ServerSideClientTextInfo:
     def dict(self):
         return self.dict_
 
-server.app.mount("/images", StaticFiles(directory="images"), name="images")
+class ServerApplication:
+    def __init__(self):
+        self.messagehistory = []
+        self.userlist = []
 
-@server.app.get("/")
+class Server:
+    def __init__(self):
+        self.fastapi = FastAPI()
+        self.connectedclients = {}
+        self.deadclients = []
+
+        self.clientadded = Event()
+        self.clientremoved = Event()
+
+        self.application = ServerApplication()
+
+    async def connectclient(self, client):
+        self.connectedclients[client.websocket] = client
+        await self.clientadded.fire(client)
+
+    async def disconnectclient(self, client):
+        if client.websocket in self.connectedclients:
+            del self.connectedclients[client.websocket]
+            await self.clientremoved.fire(client)
+
+    async def sendtexteachclient(self, message : ServerSideClientTextInfo):
+        for client in list(self.connectedclients.values()):
+            try:
+                await client.sendtext(message)
+            except Exception:
+                self.killclient(client)
+
+        await self.cleanclients()
+
+    def killclient(self, client):
+        self.deadclients.append(client)
+
+    async def cleanclients(self):
+        for dead in self.deadclients:
+            await self.disconnectclient(dead)
+        self.deadclients.clear()
+
+server = Server()
+app = server.fastapi
+
+class Client:
+    def __init__(self, websocket, uuid, username):
+        self.websocket = websocket
+        self.uuid = uuid
+        self.username = username
+
+    async def sendtext(self, message : ServerSideClientTextInfo):
+        await self.websocket.send_text(message.jsonstr())
+
+    async def receivedtext(self):
+        return await self.websocket.receive_text()
+
+server.fastapi.mount("/images", StaticFiles(directory="images"), name="images")
+
+@server.fastapi.get("/")
 def homepage():
     with open("index.html") as f:
         return HTMLResponse(f.read())
 
-@server.app.websocket("/ws")
+async def clientremovedcb(client : Client):
+    message = ServerSideClientTextInfo("userlist", {"oldusername": client.username, "newusername": "", "mode": "left"})
+    if client.username in server.application.userlist:
+        server.application.userlist.remove(client.username)
+    await server.sendtexteachclient(message)
+
+server.clientremoved.connect(clientremovedcb)
+
+@server.fastapi.websocket("/ws")
 async def chat_endpoint(websocket: WebSocket):
     await websocket.accept()
-    client = Client(websocket, 0, "")
-    server.connect(client)
+    client = Client(websocket, 0, "User")
+    await server.connectclient(client)
 
-    for oldmessage in server.messagehistory:
-        await client.sendtext(oldmessage.jsonstr())
+    for oldmessage in server.application.messagehistory:
+        await client.sendtext(oldmessage)
+
+    for username in server.application.userlist:
+        message = ServerSideClientTextInfo("userlist", {"oldusername": "", "newusername": username, "mode": "joined"})
+        await client.sendtext(message)
+
+    server.application.userlist.append("User")
+
+    message = ServerSideClientTextInfo("userlist", {"oldusername": "", "newusername": "User", "mode": "joined"})
+    await server.sendtexteachclient(message)
 
     try:
         while True:
             strdata = await client.receivedtext()
             parsed = json.loads(strdata)
 
-            if parsed["type"] == "identify":
+            if parsed["type"] == "onopen":
                 client.uuid = parsed["userId"]
             if parsed["type"] == "nameset":
+                before = client.username
                 client.username = parsed["newname"]
+                message = ServerSideClientTextInfo("userlist", {"oldusername": before, "newusername": client.username, "mode": "changed"})
+                server.application.userlist[server.application.userlist.index(before)] = client.username
+                await server.sendtexteachclient(message)
             if parsed["type"] == "chat":
                 message = ServerSideClientTextInfo("chat", {"sender": client.username, "body": parsed["body"]})
-                server.messagehistory.append(message)
-                for connectedclient in server.connectedclients.values():
-                    try:
-                        await connectedclient.sendtext(message.jsonstr())
-                    except Exception:
-                        server.killclient(connectedclient)
+                server.application.messagehistory.append(message)
+                await server.sendtexteachclient(message)
 
-            server.cleanclients()
+            await server.cleanclients()
     except WebSocketDisconnect:
-        server.disconnect(client)
+        await server.disconnectclient(client)
